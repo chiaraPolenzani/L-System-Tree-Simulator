a350863f8005bc47063d56506e1460910e3733e7
Chiara Polenzani <chiara.polenzani@edu.unifi.it>
2025-03-03 16:24:53 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    open: true,  // Apre automaticamente il browser
+  },
+  root: './',  // La cartella principale del progetto (è già corretto così)
+});
